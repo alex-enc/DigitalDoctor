05bd643a9dcf7f5cdd8f7e1789555719288b5312
ALEXANDRA ENCARNACION <alex1102e@gmail.com>
2023-11-17 23:17:08 +0000

    Create sign up form and template

--- digidoc/views.py
+++ digidoc/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .forms import SignUpForm
 
 def home(request):
     return render(request, 'home.html')
 
+def chat(request):
+    return render(request, 'chat.html')
+
+def sign_up(request):
+    form = SignUpForm()
+    return render(request, 'sign_up.html', {'form' : form})
--- digitalDoctor/settings.py
+++ digitalDoctor/settings.py
@@ -124,3 +124,4 @@ AUTH_USER_MODEL = 'digidoc.User'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
