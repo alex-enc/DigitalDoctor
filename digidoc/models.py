ad50a0578c40ef61cd64c0e4e38c853d0079dc18
ALEXANDRA ENCARNACION <alex1102e@gmail.com>
2023-11-18 22:58:38 +0000

    Modify user models

--- digidoc/models.py
+++ digidoc/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(unique=True, blank=False)
