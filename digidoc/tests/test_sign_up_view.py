ea053ab5f451ffa17e5862427b26942f190ed264
ALEXANDRA ENCARNACION <alex1102e@gmail.com>
2023-11-20 21:36:23 +0000

    Modify sign_up_view tests

--- digidoc/tests/test_sign_up_view.py
+++ digidoc/tests/test_sign_up_view.py
@@ -7,16 +7,59 @@ from digidoc.models import User
 
 #sign up view tests
 class SignUpViewTestCase(TestCase):
+    fixtures = ['digidoc/tests/fixtures/default_user.json']
+
+    def setUp(self):
+        self.url = reverse('sign_up')
+        self.form_input = {
+            'first_name': 'Jane',
+            'last_name': 'Doe',
+            'username': 'janedoe@example.org',
+            'email': 'janedoe@example.org',
+            'new_password': 'Password123',
+            'password_confirmation': 'Password123'
+        }
+        self.user = User.objects.get(username='johndoe@example.org')
+
+    def test_sign_up_url(self):
+        self.assertEqual(self.url, '/sign_up')
+
     def test_get_sign_up(self):
-        url = reverse('sign_up')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'sign_up.html')
         form = response.context['form']
         self.assertTrue(isinstance(form, SignUpForm))
+        self.assertFalse(form.is_bound)
 
-    # def test_successful_sign_up(self):
+    def test_succesful_sign_up(self):
+        before_count = User.objects.count()
+        response = self.client.post(self.url, self.form_input, follow=True)
+        after_count = User.objects.count()
+        self.assertEqual(after_count, before_count+1)
+        response_url = reverse('chat')
+        self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
+        self.assertTemplateUsed(response, 'chat.html')
+        user = User.objects.get(username='johndoe@example.org')
+        self.assertEqual(user.first_name, 'John')
+        self.assertEqual(user.last_name, 'Doe')
+        self.assertEqual(user.email, 'johndoe@example.org')
+        is_password_correct = check_password('Password123', user.password)
+        self.assertTrue(is_password_correct)
+        # self.assertTrue(self._is_logged_in())
+
+    def test_successful_sign_up(self):
+        response = self.client.post(self.url, self.form_input, follow=True)
+        response_url = reverse('chat')
+        self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
+        self.assertTemplateUsed(response, 'chat.html')
+        
+    # def test_post_sign_up_redirects_when_logged_in(self):
+    #     self.client.login(username=self.user.username, password='Password123')
+    #     before_count = User.objects.count()
     #     response = self.client.post(self.url, self.form_input, follow=True)
-    #     response_url = reverse('chat')
-    #     self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
+    #     after_count = User.objects.count()
+    #     self.assertEqual(after_count, before_count)
+    #     redirect_url = reverse('chat')
+    #     self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
     #     self.assertTemplateUsed(response, 'chat.html')
